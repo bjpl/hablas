8dbeaa3f36e3d7d0c4f881db617fb06dae48a6de
bjpl <brandon.lambert87@gmail.com>
2025-10-28 23:32:21 -0700

    fix: Clean remaining duplicate audioUrl on resource 2

--- data/resources.ts
+++ data/resources.ts
@@ -60,8 +60,7 @@ export const resources: Resource[] = [
     ],
     "offline": true,
     "audioUrl": "/audio/resource-2.mp3",
-    "contentPath": "C:\\Users\\brand\\Development\\Project_Workspace\\active-development\\hablas\\generated-resources\\50-batch\\repartidor\\basic_audio_1-audio-script.txt",
-    "audioUrl": "/audio/resource-2.mp3"
+    "contentPath": "C:\\Users\\brand\\Development\\Project_Workspace\\active-development\\hablas\\generated-resources\\50-batch\\repartidor\\basic_audio_1-audio-script.txt"
   },
   {
     "id": 3,
